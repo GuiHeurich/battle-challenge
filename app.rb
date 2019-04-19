bfce05d56daf472b32811fe94deb82f46dc90053
guilhe0756 <guiheurich@gmail.com>
2019-04-19 10:20:17 +0100

    Although this is my first commit, it contains several solved challenges of Battle. So far, I have dealt with the following user stories:
    As Player 1,
    So I can start to win a game of Battle,
    I want my attack to reduce Player 2's HP by 10
    
    As two Players,
    So we can continue our game of Battle,
    We want to switch turns
    
    As Player 1,
    So I can see how close I am to losing,
    I want to see my own hit points
    
    As Player 1,
    So I can lose a game of Battle,
    I want Player 2 to attack me, and I want to get a confirmation
    
    As Player 1,
    So I can start to lose a game of Battle,
    I want Player 2's attack to reduce my HP by 10

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'sinatra/base'
+require_relative 'player'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    $p1 = Player.new(params[:p1_name])
+    $p2 = Player.new(params[:p2_name])
+    redirect '/play'
+  end
+
+  get '/play' do
+    @p1_name = $p1.name
+    @p2_name = $p2.name
+    @p1_hp = 100
+    @p2_hp = 100
+    erb :play
+  end
+
+  post '/attack' do
+    redirect '/result'
+  end
+
+  get '/result' do
+    @p1_name = $p1.name
+    @p2_name = $p2.name
+    @p1_hp = 100
+    @p2_hp = 80
+    erb :result
+  end
+
+  run! if app_file == $0
+end
